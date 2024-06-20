81b4c5e307a404bbd5fac227af9ffacb91ec5f70
Marcel Stadelmann <m@marceli.to>
2024-06-20 16:55:13 +0200

    wip

--- resources/views/pages/order/summary.blade.php
+++ resources/views/pages/order/summary.blade.php
@@ -8,7 +8,39 @@
     <x-order.menu order_step="{{ $order_step }}" />
   </div>
   <div class="md:col-span-6 lg:col-span-5 xl:col-span-4">
-
+    <x-table.row class="font-europa-bold font-bold">
+      <span>Warenkorb</span>
+    </x-table.row>
+    @foreach ($cart['items'] as $item)
+    <div class="mb-32 last-of-type:mb-0 divide-y divide-black border-t border-t-black mt-1">
+      <div class="grid grid-cols-4">
+        <x-table.row class="border-none col-span-4 md:col-span-3 flex justify-between">
+          <span>{{ $item['title'] }}</span>
+        </x-table.row>
+        <x-table.row class="border-none col-span-4 md:col-span-1 flex justify-end">
+          <span>{{ $item['quantity'] }}</span>
+        </x-table.row>
+      </div>
+      <div class="grid grid-cols-4">
+        <x-table.row class="border-none col-span-3 md:col-span-3">
+          <span>{{ $item['description'] }}</span>
+        </x-table.row>
+        <x-table.row class="border-none col-span-1 flex justify-between 2xl:pl-16">
+          <span>CHF</span>
+          <span>{{ $item['price'] }}</span>
+        </x-table.row>
+      </div>
+      <div class="grid grid-cols-4 !border-b border-b-black">
+        <x-table.row class="border-none col-span-3 md:col-span-3">
+          <span>Verpackung und Versand</span>
+        </x-table.row>
+        <x-table.row class="border-none col-span-1 flex justify-between 2xl:pl-16">
+          <span>CHF</span>
+          <span>{{ $item['shipping'] }}</span>
+        </x-table.row>
+      </div>
+    </div>
+    @endforeach
   </div>
 </div>
 @endsection
