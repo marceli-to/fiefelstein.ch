81bb0e09dbe9382df48dffbcadf4fe2b52c62464
Marcel Stadelmann <m@marceli.to>
2024-07-08 09:20:32 +0200

    wip

--- resources/views/components/product/cards/teaser.blade.php
+++ resources/views/components/product/cards/teaser.blade.php
@@ -1,7 +1,6 @@
 @props(['product'])
-@if ($product)
 <a 
-  href="{{ route('product.show', ['product' => $product->slug]) }}"
+  href="{{ route('product.show', ['product' => $product->slug ?? '-']) }}"
   title="{{ $product->title }}"
   class="relative">
   <h2 class="absolute top-12 lg:top-70 left-16 lg:left-[calc((100vw/12)_-_2px)] text-lg">
@@ -9,4 +8,3 @@
   </h2>
   <x-media.picture :image="$product->image" :alt="$product->title" />
 </a>
-@endif
