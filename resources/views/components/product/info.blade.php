48f0b99d3c2ebf127b8613d37c7dc1cd92d9b8d4
Marcel Stadelmann <m@marceli.to>
2024-07-15 14:58:58 +0200

    wip

--- resources/views/components/product/info.blade.php
+++ resources/views/components/product/info.blade.php
@@ -3,29 +3,29 @@
   {{ $attributes->merge(['class' => 'bg-white w-full mt-32 lg:mt-0 lg:pl-16 lg:absolute lg:h-full lg:z-30 lg:top-0 lg:left-[calc((100%_/_12)_-_15px)] lg:w-[calc((100%/6)_+_19px)]']) }}
   data-variation-wrapper="{{ $product->uuid }}">
   <div class="lg:pr-16 lg:max-h-[calc(100vh_-_220px)] lg:overflow-y-auto">
-    <x-table.row class="font-europa-bold font-bold">
+    <x-table.row class="font-europa-bold font-bold py-4">
       {{ $product->title }}
     </x-table.row>
-    <x-table.row>
+    <x-table.row class="py-4">
       {{ $product->description }}
     </x-table.row>
 
     @foreach($product->attributes as $attribute)
-      <x-table.row>
+      <x-table.row class="py-4">
         {{ $attribute }}
       </x-table.row>
     @endforeach
 
-    <x-table.row>
+    <x-table.row class="py-4">
       CHF {{ $product->price }}
     </x-table.row>
 
     @if ($product->state->value() == 'deliverable' || $product->state->value() == 'ready_for_pickup')
-      <x-table.row class="italic border-b border-b-black">
+      <x-table.row class="italic border-b border-b-black py-4">
         {{ $product->stock }} Stück {{ $product->state->label() }}
       </x-table.row>
     @else
-    <x-table.row class="italic border-b border-b-black">
+    <x-table.row class="italic border-b border-b-black py-4">
       {{ $product->stateText }}
     </x-table.row>
     @endif
