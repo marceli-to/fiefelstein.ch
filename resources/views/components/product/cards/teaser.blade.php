5e38ee06e35cff44de1f228665ce562c37b29235
Marcel Stadelmann <m@marceli.to>
2024-07-15 14:02:13 +0200

    wip

--- resources/views/components/product/cards/teaser.blade.php
+++ resources/views/components/product/cards/teaser.blade.php
@@ -2,7 +2,7 @@
 <a 
   href="{{ route('product.show', ['product' => $product->slug ?? '-']) }}"
   title="{{ $product->group_title }}"
-  class="relative">
+  class="relative block">
   <h2 class="absolute w-[calc(100%_-_32px)] z-50 top-12 lg:top-70 left-16 lg:left-[calc((100vw/12)_-_2px)] text-lg">
     {{ $product->group_title }}
   </h2>
